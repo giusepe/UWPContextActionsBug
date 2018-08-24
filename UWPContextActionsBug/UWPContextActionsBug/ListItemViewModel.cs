e790256e6261f984ca853840a6694c7e0c7440af
Giusepe Casagrande <giusepe@gmail.com>
2018-08-24 14:50:13 +0200

    First Commit

---
+++ UWPContextActionsBug/UWPContextActionsBug/ListItemViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace UWPContextActionsBug
+{
+    public class ListItemViewModel
+    {
+        public ListItemViewModel()
+        {
+            ToggleMarkAsRead = new Command(toggleIsRead, CanExecute);
+            Remove = new Command(remove, CanExecute);
+        }
+
+        public string Description { get { return $"This is desc of item {Number}."; } }
+
+        public string IsRead { get; set; } = "No";
+
+        public int Number { get; set; }
+
+        public Command ToggleMarkAsRead { get; set; }
+
+        public Command Remove { get; set; }
+
+        public bool CanExecute()
+        {
+            return true;
+        }
+
+        private void toggleIsRead()
+        {
+            IsRead = ((IsRead == "Yes") ? "No" : "Yes");
+            MessagingCenter.Send<ListItemViewModel, int>(this, "update", Number);
+        }
+
+        private void remove()
+        {
+            MessagingCenter.Send<ListItemViewModel, ListItemViewModel>(this, "remove", this);
+        }
+    }
+}
---
+++ UWPContextActionsBug/UWPContextActionsBug/MainPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace UWPContextActionsBug
+{
+    public class MainPageViewModel
+    {
+        private const int initialItems = 3;
+        private int newItemNumber = 1;
+
+        public MainPageViewModel()
+        {
+            MessagingCenter.Subscribe<ListItemViewModel, int>(this, "update", (sender, arg) => updateAnItem(arg));
+            MessagingCenter.Subscribe<ListItemViewModel, ListItemViewModel>(this, "remove", (sender, arg) => removeAnItem(arg));
+
+            AddListItemCommand = new Command(addListItem);
+            for (int i = 0; i < initialItems; i++)
+            {
+                ItemCollection.Add(new ListItemViewModel() { Number = newItemNumber });
+                newItemNumber++;
+            }
+        }
+
+        public ObservableCollection<ListItemViewModel> ItemCollection { get; set; } = new ObservableCollection<ListItemViewModel>();
+        public Command AddListItemCommand { get; set; }
+
+        private void updateAnItem(int number)
+        {
+            var currVm = ItemCollection.FirstOrDefault(vm => vm.Number == number);
+            var indexOfVM = ItemCollection.IndexOf(currVm);
+            ItemCollection[indexOfVM] = new ListItemViewModel
+            {
+                Number = currVm.Number,
+                IsRead = currVm.IsRead
+            };
+        }
+
+        private void removeAnItem(ListItemViewModel item)
+        {
+            ItemCollection.Remove(item);
+        }
+
+        private void addListItem()
+        {
+            var newItem = new ListItemViewModel() { Number = newItemNumber };
+            newItemNumber++;
+            ItemCollection.Add(newItem);
+        }
+    }
+}
